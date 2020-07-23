40f1f3da612746e7995230485b1926d5af9ac125
cleberson-dev <cleberson.dev@gmail.com>
2020-07-23 09:52:46 -0300

    Set up Vuex store

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Counter: <span>{{ counter }}</span></p>
+    <button @click="increment">+</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    increment() {
+      this.$store.commit('increment');
+    }
+  },
+  computed: {
+    counter() {
+      return this.$store.getters.counter;
+    }
   }
 }
 </script>
