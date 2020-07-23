b56aaf354f9fca882ab6d510fbca32489626b400
cleberson-dev <cleberson.dev@gmail.com>
2020-07-23 12:20:48 -0300

    Created NavLink component

---
+++ src/components/NavLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link :to="path">
+    <slot />
+    {{title}}
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+  a {
+    text-decoration: none;
+    color: #554967;
+  }
+</style>
