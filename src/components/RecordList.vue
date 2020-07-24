8bba78f4ff3abb7d0b34d8fbd8dd656a0d9f379b
cleberson-dev <cleberson.dev@gmail.com>
2020-07-24 11:39:23 -0300

    Created RecordList component

---
+++ src/components/RecordList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul v-if="records">
+    <record-item
+      v-for="record in records"
+      :key="record.id"
+      :title="record.title"
+      :postedAt="record.postedAt"
+      :mood="record.mood"
+      :tasks="record.tasks"
+    />
+  </ul>
+</template>
+
+<script>
+import RecordItem from './RecordItem';
+
+export default {
+  components: {
+    RecordItem
+  },
+  props: {
+    records: Array
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  padding-left: 0;
+}
+
+ul > li {
+  margin-bottom: 10px;
+}
+</style>
