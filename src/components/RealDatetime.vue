15f96162f904a7dfc00d53768233ccf30dabf433
cleberson-dev <cleberson.dev@gmail.com>
2020-07-23 22:21:39 -0300

    Created RealDatetime component

---
+++ src/components/RealDatetime.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="real-datetime">
+    <time :datetime="formattedTime" class="time">{{ formattedTime }}</time>
+    <time :datetime="formattedDate" class="date">{{ formattedDate }}</time>
+  </div>
+</template>
+
+<script>
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+export default {
+  data() {
+    return {
+      datetime: new Date()
+    }
+  },
+  computed: {
+    formattedDate() {
+      const formatText = "d 'de' MMMM 'de' yyyy";
+      return format(
+        this.datetime, 
+        formatText, 
+        { locale: ptBR }
+      );
+    },
+    formattedTime() {
+      const { datetime } = this;
+
+      const hh = `${datetime.getHours()}`.padStart(2, '0');  
+      const mm = `${datetime.getMinutes()}`.padStart(2, '0');  
+
+      return `${hh}:${mm}`;
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.datetime = new Date();
+    }, 1000);
+  }
+}
+</script>
+
+<style scoped>
+.real-datetime {
+  text-align: right;
+}
+
+.real-datetime time {
+  font-weight: 700;
+  display: block;
+}
+
+.real-datetime .time {
+  color: #9C89B8;
+  font-size: 1.3rem;
+}
+
+.real-datetime .date {
+  color: #B8BEDD;
+}
+</style>
