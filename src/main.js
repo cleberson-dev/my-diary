9fcfe02cf8d8748598d7f7bc7a7eec7cadb9172d
cleberson-dev <cleberson.dev@gmail.com>
2020-07-25 00:27:14 -0300

    WIP: Added Records page

--- src/main.js
+++ src/main.js
@@ -4,19 +4,17 @@ import App from './App.vue'
 import store from './store';
 
 import HomePage from './pages/Home';
-import AboutPage from './pages/About';
+import RecordsPage from './pages/Records';
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/about', component: AboutPage }
+  { path: '/registros', component: RecordsPage }
 ];
 
-const router = new VueRouter({
-  routes
-});
+const router = new VueRouter({ routes });
 
 new Vue({
   render: h => h(App),
---
+++ src/pages/Records.vue
@@ -0,0 +1,63 @@
+<template>
+  <main>
+    <h1 class="title">Meus Registros</h1>
+    <record-list :records="records" />
+  </main>
+</template>
+
+<script>
+import RecordList from '../components/RecordList';
+
+export default {
+  name: 'RecordsPage',
+  components: {
+    RecordList
+  },
+  data() {
+    return {
+      records: [
+        { 
+          id: 1,
+          title: 'Estresse é sempre presente',
+          postedAt: new Date(),
+          mood: 'happy',
+          tasks: { completed: 5, total: 10 }
+        },
+        { 
+          id: 2,
+          title: 'Estresse é sempre presente',
+          postedAt: new Date(),
+          mood: 'sad',
+          tasks: { completed: 1, total: 2 }
+        },
+        { 
+          id: 3,
+          title: 'Estresse é sempre presente',
+          postedAt: new Date(),
+          mood: 'tired',
+          tasks: { completed: 3, total: 10 }
+        },
+        { 
+          id: 4,
+          title: 'Estresse é sempre presente',
+          postedAt: new Date(),
+          mood: 'sad',
+          tasks: { completed: 6, total: 6 }
+        },
+        { 
+          id: 5,
+          title: 'Estresse é sempre presente',
+          postedAt: new Date(),
+          tasks: { completed: 2, total: 4 }
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  main > .title {
+    margin-left: 1.25rem;
+  }
+</style>
